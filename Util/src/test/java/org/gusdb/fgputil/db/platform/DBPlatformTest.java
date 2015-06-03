4b0006bbbe9a53a3d26b6a501bb3da080272a869
Ryan Doherty <tech@conical.org>
2015-06-03 18:20:39 +0000

    Remove unnecessarily documented warnings and unnecessary Exception declarations (Java 7)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/branches/oauth-login@16135 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/test/java/org/gusdb/fgputil/db/platform/DBPlatformTest.java
+++ Util/src/test/java/org/gusdb/fgputil/db/platform/DBPlatformTest.java
@@ -31,7 +31,7 @@ public class DBPlatformTest {
 
   // Cannot make this a unit test because it would require both Postgres and Oracle drivers to succeed
   //@Test
-  public void testDbPlatform() throws Exception {
+  public void testDbPlatform() {
     ConnectionPoolConfig appConfig = new OracleConfig();
     ConnectionPoolConfig userConfig = new PostgresConfig();
 
