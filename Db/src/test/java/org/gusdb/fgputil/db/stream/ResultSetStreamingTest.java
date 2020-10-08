e1e0df9aa5de6b73ee38ee6b9c67da3665363e15
Ryan Doherty <rdoherty@upenn.edu>
2020-10-08 00:10:28 -0400

    fix up formatting, APIs, comment

--- Db/src/test/java/org/gusdb/fgputil/db/stream/ResultSetStreamingTest.java
+++ Db/src/test/java/org/gusdb/fgputil/db/stream/ResultSetStreamingTest.java
@@ -1,5 +1,7 @@
 package org.gusdb.fgputil.db.stream;
 
+import static org.gusdb.fgputil.FormatUtil.NL;
+
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -27,6 +29,22 @@ public class ResultSetStreamingTest {
 
   private static final String DB_SETUP_SCRIPT = "org/gusdb/fgputil/db/runner/streamingTestSetup.sql";
 
+  private static final String[] EXPECTED_OUTPUT = {
+      "infra: [ ryan, ellie, steve ]",
+      "ui: [ dave, jamie, cristina ]",
+      "dd: [ johnb, bindu, haiming ]",
+      "outreach: [ brian, omar, suzanne ]"
+  };
+
+  // class will represent a row from the "records" table in our DB
+  private static class Person extends ThreeTuple<Integer, String, String> {
+    public Person(int id, String group, String name) {
+      super(id, group, name);
+    }
+    public String getName() { return getThird(); }
+    public String getGroup() { return getSecond(); }
+  }
+
   private DataSource _ds;
 
   @Before
@@ -35,39 +53,29 @@ public class ResultSetStreamingTest {
     SqlScriptRunner.runSqlScript(_ds, DB_SETUP_SCRIPT);
   }
 
-  private static class Person extends ThreeTuple<Integer, String, String>{
-    public Person(int id, String group, String name) {
-      super(id, group, name);
-    }
-    public String getName() { return getThird(); }
-    public String getGroup() { return getSecond(); }
-  }
-
-  private static final String[] EXPECTED_OUTPUT = {
-      "infra: [ ryan, ellie, steve ]",
-      "ui: [ dave, jamie, cristina ]",
-      "dd: [ johnb, bindu, haiming ]",
-      "outreach: [ brian, omar, suzanne ]"
-  };
-
   @Test
   public void streamingTest() throws IOException {
 
     // create an output stream to capture the output from our streamer
     ByteArrayOutputStream capturedOutput = new ByteArrayOutputStream();
 
-    writeAggregatedData(capturedOutput);
+    // stream DB rows into an aggregator, format, and write to an output stream
+    writeAggregatedData(_ds, capturedOutput);
 
-    String[] aggregatedRows = capturedOutput.toString().split("\\n");
+    // read the output from the stream for analysis
+    String[] outputRows = capturedOutput.toString().split("\\n");
 
-    Assert.assertEquals(EXPECTED_OUTPUT.length, aggregatedRows.length);
+    // same number of rows as expected?
+    Assert.assertEquals(EXPECTED_OUTPUT.length, outputRows.length);
+
+    // row values same as expected?
     for (int i = 0; i < EXPECTED_OUTPUT.length; i++) {
-      System.out.println(aggregatedRows[i]);
-      Assert.assertEquals(EXPECTED_OUTPUT[i], aggregatedRows[i]);
+      System.out.println(outputRows[i]);
+      Assert.assertEquals(EXPECTED_OUTPUT[i], outputRows[i]);
     }
   }
 
-  private void writeAggregatedData(OutputStream out) throws IOException {
+  private static void writeAggregatedData(DataSource ds, OutputStream out) throws IOException {
 
     // SQL to get rows from our test DB
     String sql = "select * from records";
@@ -77,20 +85,19 @@ public class ResultSetStreamingTest {
       ": [ " + people.stream().map(Person::getName).collect(Collectors.joining(", ")) + " ]";
 
     try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
-      new SQLRunner(_ds, sql).executeQuery(rs -> {
+      new SQLRunner(ds, sql).executeQuery(rs -> {
         try {
           // construct an iterator over objects constructed by the rows returned by the query
-          Iterator<Person> people = new ResultSetIterator<>(rs, row ->
-            Optional.of(new Person(row.getInt(1), row.getString(2), row.getString(3))));
+          Iterator<Person> people = new ResultSetIterator<>(rs,
+              row -> Optional.of(new Person(row.getInt(1), row.getString(2), row.getString(3))));
 
           // group the objects by a common value
           Iterator<List<Person>> groups = new GroupingIterator<Person>(people,
-            (p1, p2) -> p1.getGroup().equals(p2.getGroup()));
+              (p1, p2) -> p1.getGroup().equals(p2.getGroup()));
 
           // iterate through groups and format into strings to be written to stream
           for (List<Person> group : IteratorUtil.toIterable(groups)) {
-            writer.write(formatter.apply(group));
-            writer.newLine();
+            writer.write(formatter.apply(group) + NL);
           }
 
           writer.flush();
