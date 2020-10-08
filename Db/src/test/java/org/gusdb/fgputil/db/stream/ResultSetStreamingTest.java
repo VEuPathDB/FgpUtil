846bad923290f8ee500c23895d7175b2381ba257
Ryan Doherty <rdoherty@upenn.edu>
2020-10-07 23:55:49 -0400

    Refine and convert to true unit test

--- Db/src/test/java/org/gusdb/fgputil/db/stream/ResultSetStreamingTest.java
+++ Db/src/test/java/org/gusdb/fgputil/db/stream/ResultSetStreamingTest.java
@@ -1,7 +1,9 @@
 package org.gusdb.fgputil.db.stream;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Iterator;
 import java.util.List;
@@ -17,6 +19,7 @@ import org.gusdb.fgputil.db.runner.SQLRunner;
 import org.gusdb.fgputil.iterator.GroupingIterator;
 import org.gusdb.fgputil.iterator.IteratorUtil;
 import org.gusdb.fgputil.test.TestUtil;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -40,8 +43,31 @@ public class ResultSetStreamingTest {
     public String getGroup() { return getSecond(); }
   }
 
+  private static final String[] EXPECTED_OUTPUT = {
+      "infra: [ ryan, ellie, steve ]",
+      "ui: [ dave, jamie, cristina ]",
+      "dd: [ johnb, bindu, haiming ]",
+      "outreach: [ brian, omar, suzanne ]"
+  };
+
   @Test
   public void streamingTest() throws IOException {
+
+    // create an output stream to capture the output from our streamer
+    ByteArrayOutputStream capturedOutput = new ByteArrayOutputStream();
+
+    writeAggregatedData(capturedOutput);
+
+    String[] aggregatedRows = capturedOutput.toString().split("\\n");
+
+    Assert.assertEquals(EXPECTED_OUTPUT.length, aggregatedRows.length);
+    for (int i = 0; i < EXPECTED_OUTPUT.length; i++) {
+      System.out.println(aggregatedRows[i]);
+      Assert.assertEquals(EXPECTED_OUTPUT[i], aggregatedRows[i]);
+    }
+  }
+
+  private void writeAggregatedData(OutputStream out) throws IOException {
 
     // SQL to get rows from our test DB
     String sql = "select * from records";
@@ -50,7 +76,7 @@ public class ResultSetStreamingTest {
     Function<List<Person>,String> formatter = people -> people.get(0).getGroup() +
       ": [ " + people.stream().map(Person::getName).collect(Collectors.joining(", ")) + " ]";
 
-    try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out))) {
+    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
       new SQLRunner(_ds, sql).executeQuery(rs -> {
         try {
           // construct an iterator over objects constructed by the rows returned by the query
@@ -63,10 +89,11 @@ public class ResultSetStreamingTest {
 
           // iterate through groups and format into strings to be written to stream
           for (List<Person> group : IteratorUtil.toIterable(groups)) {
-            out.write(formatter.apply(group));
-            out.newLine();
+            writer.write(formatter.apply(group));
+            writer.newLine();
           }
 
+          writer.flush();
           return null;
         }
         catch (IOException e) {
