cf7f188bb0499d71e8a9081b34f1a425ad880dff
Ryan Doherty <rdoherty@upenn.edu>
2020-10-29 16:48:48 -0400

    Add new handler for single column ints

---
+++ Db/src/main/java/org/gusdb/fgputil/db/runner/SingleIntResultSetHandler.java
@@ -0,0 +1,33 @@
+package org.gusdb.fgputil.db.runner;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.gusdb.fgputil.db.runner.SQLRunner.ResultSetHandler;
+
+/**
+ * Handles a result containing a single column with an integer value.  Similar
+ * to SingleLongResultSetHandler except:
+ * - throws NoSuchElementException if the SQL executed does not provide a row,
+ *       or a row with no columns, or a null value in the column
+ * - throws ArithmeticException if the number provided does not fit into an Integer
+ *
+ * @author rdoherty
+ */
+public class SingleIntResultSetHandler implements ResultSetHandler<Integer> {
+
+  /**
+   * Handles a result containing a single column with an integer value.  Similar
+   * to SingleLongResultSetHandler except throws exceptions in common error cases.
+   * 
+   * @param rs result set to be read
+   * @throws NoSuchElementException if the SQL executed does not provide a row,
+   * or a row with no columns, or a null value in the column
+   * @throws ArithmeticException if the number provided does not fit into an Integer
+   */
+  @Override
+  public Integer handleResult(ResultSet rs) throws SQLException {
+    return Math.toIntExact(new SingleLongResultSetHandler().handleResult(rs).orElseThrow());
+  }
+
+}
