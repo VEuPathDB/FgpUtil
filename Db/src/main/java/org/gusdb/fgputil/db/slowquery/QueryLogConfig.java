96197400e32d108cd9d409c43afe60eae0068dde
Ryan Doherty <rdoherty@upenn.edu>
2021-01-29 12:31:54 -0500

    Give all methods default implementations for trivial applications

--- Db/src/main/java/org/gusdb/fgputil/db/slowquery/QueryLogConfig.java
+++ Db/src/main/java/org/gusdb/fgputil/db/slowquery/QueryLogConfig.java
@@ -2,9 +2,21 @@ package org.gusdb.fgputil.db.slowquery;
 
 public interface QueryLogConfig {
 
-  public double getBaseline();
-  public double getSlow();
-  public boolean isIgnoredSlow(String sql);
-  public boolean isIgnoredBaseline(String sql);
-  
+  public static final double DEFAULT_BASELINE = 0.1;
+  public static final double DEFAULT_SLOW = 5;
+
+  default double getBaseline() { return DEFAULT_BASELINE; }
+
+  default double getSlow() { return DEFAULT_SLOW; }
+
+  /**
+   * @param sql sql to decide whether to ignore or not
+   */
+  default boolean isIgnoredSlow(String sql) { return false; }
+
+  /**
+   * @param sql sql to decide whether to ignore or not
+   */
+  default boolean isIgnoredBaseline(String sql) { return false; }
+
 }
