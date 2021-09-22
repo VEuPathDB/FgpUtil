3a7c5dd1c6b4c52679de3a2450eb4a2985808504
Ryan Doherty <rdoherty@upenn.edu>
2021-09-22 06:25:39 -0400

    Add MIME type for tab-separated text

--- Web/src/main/java/org/gusdb/fgputil/web/MimeTypes.java
+++ Web/src/main/java/org/gusdb/fgputil/web/MimeTypes.java
@@ -10,4 +10,6 @@ public class MimeTypes {
   // newline-delimited JSON for easy streaming of records
   public static final String ND_JSON = "application/x-ndjson";
 
+  // tabular text
+  public static final String TEXT_TABULAR = "text/tab-separated-values";
 }
