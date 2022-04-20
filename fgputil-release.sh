5d00fde6b9592b7fd9c7e010fdb94cb91efa30df
Ryan Doherty <tech@conical.org>
2022-04-20 01:19:47 -0400

    Avoid changing perms

--- fgputil-release.sh
+++ fgputil-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download standard release script and execute interactively
-curl -O https://raw.githubusercontent.com/VEuPathDB/base-pom/main/release.sh && ./release.sh
+curl -O https://raw.githubusercontent.com/VEuPathDB/base-pom/main/release.sh && bash release.sh
 
 # remove downloaded file regardless of success
 rm -f release.sh
