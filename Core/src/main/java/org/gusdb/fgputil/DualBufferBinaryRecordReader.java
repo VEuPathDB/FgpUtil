0c1001a20d8b62147b54d5fefd8c5518abaa5d76
Dan Galdi <dmgaldi@upenn.edu>
2022-12-12 16:47:23 -0500

    Keep track of time spent waiting for IO

--- Core/src/main/java/org/gusdb/fgputil/DualBufferBinaryRecordReader.java
+++ Core/src/main/java/org/gusdb/fgputil/DualBufferBinaryRecordReader.java
@@ -12,7 +12,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import org.gusdb.fgputil.iterator.OptionStream;
 
@@ -39,6 +38,7 @@ public class DualBufferBinaryRecordReader<T> implements OptionStream<T>, AutoClo
   private ByteBuffer _current;
   private ByteBuffer _next;
   private Future<Integer> _nextFill;
+  private Timer _awaitingFillTimer = null;
 
   /**
    * Creates a instance that will read from the passed file, expecting records of size recordLength
@@ -88,6 +88,10 @@ public class DualBufferBinaryRecordReader<T> implements OptionStream<T>, AutoClo
     return Optional.of(_deserializer.apply(_current));
   }
 
+  public long getTimeAwaitingFill() {
+    return _awaitingFillTimer.getElapsed();
+  }
+
   private void startNextFill() {
     _next.clear();
     _nextFill = _channel.read(_next, _fileCursor);
@@ -97,7 +101,13 @@ public class DualBufferBinaryRecordReader<T> implements OptionStream<T>, AutoClo
   private void resetCurrent() {
     try {
       // wait for the fill to complete (hopefully will be ready immediately)
+      if (_awaitingFillTimer == null){
+        _awaitingFillTimer = Timer.start();
+      } else {
+        _awaitingFillTimer.resume();
+      }
       Integer bytesRead = _nextFill.get();
+      _awaitingFillTimer.pause();
 
       // throw if bytes does not represent an exact number of records
       if (bytesRead != -1 && bytesRead % _recordLength != 0) {
