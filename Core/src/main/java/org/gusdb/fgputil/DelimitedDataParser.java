171d78313a5f00142ae2b6d47c7f93c86b6d2733
Ryan Doherty <tech@conical.org>
2023-01-31 17:29:25 -0500

    Enhancement to fixed size map and make use of it in DelimitedDataParser

--- Core/src/main/java/org/gusdb/fgputil/DelimitedDataParser.java
+++ Core/src/main/java/org/gusdb/fgputil/DelimitedDataParser.java
@@ -1,10 +1,11 @@
 package org.gusdb.fgputil;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import org.gusdb.fgputil.collection.FixedSizeStringMap;
 
 /**
  * Facilitates conversion of lines of delimited data into named columns.  The
@@ -15,24 +16,24 @@ import java.util.Optional;
  */
 public class DelimitedDataParser {
 
-  private final String[] _columnNames;
+  private final FixedSizeStringMap.Builder _lineBuilder;
   private final String _delimiterRegex;
   private final boolean _enforceStrictColumns;
 
   public DelimitedDataParser(List<String> columnNames, String delimiterRegex, boolean enforceStrictColumns) {
     _delimiterRegex = delimiterRegex;
     _enforceStrictColumns = enforceStrictColumns;
-    _columnNames = columnNames.toArray(new String[columnNames.size()]);
+    _lineBuilder = new FixedSizeStringMap.Builder(columnNames.toArray(new String[columnNames.size()]));
   }
 
   public DelimitedDataParser(String headerLine, String delimiterRegex, boolean enforceStrictColumns) {
     _delimiterRegex = delimiterRegex;
     _enforceStrictColumns = enforceStrictColumns;
-    _columnNames = headerLine.split(delimiterRegex);
+    _lineBuilder = new FixedSizeStringMap.Builder(headerLine.split(delimiterRegex));
   }
 
   public List<String> getColumnNames() {
-    return Collections.unmodifiableList(Arrays.asList(_columnNames));
+    return new ArrayList<>(_lineBuilder.keySet());
   }
 
   public Optional<Integer> indexOfColumn(String columnName) {
@@ -42,19 +43,13 @@ public class DelimitedDataParser {
 
   public String[] parseLineToArray(String line) {
     String[] values = line.split(_delimiterRegex, -1);
-    if (_enforceStrictColumns && values.length != _columnNames.length) {
-      throw new RuntimeException("Expected " + _columnNames.length + " columns but parsed " + values.length + " in line:\n" + line);
+    if (_enforceStrictColumns && values.length != _lineBuilder.size()) {
+      throw new RuntimeException("Expected " + _lineBuilder.size() + " columns but parsed " + values.length + " in line:\n" + line);
     }
     return values;
   }
 
-  public LinkedHashMap<String,String> parseLine(String line) {
-    String[] values = parseLineToArray(line);
-    LinkedHashMap<String,String> map = new LinkedHashMap<>();
-    int numToParse = Math.min(values.length, _columnNames.length);
-    for (int i = 0; i < numToParse; i++) {
-      map.put(_columnNames[i], values[i]);
-    }
-    return map;
+  public Map<String,String> parseLine(String line) {
+    return _lineBuilder.build().putAll(parseLineToArray(line));
   }
 }
--- Core/src/main/java/org/gusdb/fgputil/collection/FixedSizeStringMap.java
+++ Core/src/main/java/org/gusdb/fgputil/collection/FixedSizeStringMap.java
@@ -2,10 +2,8 @@ package org.gusdb.fgputil.collection;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,13 +15,23 @@ public class FixedSizeStringMap implements Map<String,String> {
 
     private final Map<String,Integer> _keyIndex;
 
-    public Builder(List<String> keys) {
-      if (keys.isEmpty())
+    public Builder(String[] keys) {
+      if (keys == null || keys.length == 0)
         throw new IllegalArgumentException("FixedSizeStringMap must have at least one key.");
-      _keyIndex = new HashMap<>();
-      for (int i = 0; i < keys.size(); i++) {
-        _keyIndex.put(keys.get(i), i);
+      _keyIndex = new LinkedHashMap<>();
+      for (int i = 0; i < keys.length; i++) {
+        if (keys[i] == null)
+          throw new IllegalArgumentException("No keys can be null.");
+        _keyIndex.put(keys[i], i);
       }
+    }
+
+    public Set<String> keySet() {
+      return _keyIndex.keySet();
+    }
+
+    public int size() {
+      return _keyIndex.size();
     }
 
     public FixedSizeStringMap build() {
@@ -38,10 +46,11 @@ public class FixedSizeStringMap implements Map<String,String> {
     _keyIndex = keyIndex;
   }
 
-  public void putAll(String[] values) {
-    if (values.length != _keyIndex.size())
-      throw new IllegalArgumentException("Only arrays of size " + _keyIndex.size() + " are allowed.");
+  public FixedSizeStringMap putAll(String[] values) {
+    if (values.length > _keyIndex.size())
+      throw new IllegalArgumentException("Only arrays of size " + _keyIndex.size() + " or smaller are allowed.");
     _values = values;
+    return this;
   }
 
   @Override
@@ -62,7 +71,7 @@ public class FixedSizeStringMap implements Map<String,String> {
   @Override
   public boolean containsValue(Object value) {
     if (_values == null) return false;
-    for (int i = 0; i < _keyIndex.size(); i++) {
+    for (int i = 0; i < _values.length; i++) {
       if (_values[i] != null && _values[i].equals(value))
         return true;
     }
@@ -73,7 +82,7 @@ public class FixedSizeStringMap implements Map<String,String> {
   public String get(Object key) {
     if (_values == null) return null;
     Integer index = _keyIndex.get(key);
-    return index == null ? null : _values[index];
+    return index == null ? null : index >= _values.length ? null : _values[index];
   }
 
   @Override
@@ -81,10 +90,18 @@ public class FixedSizeStringMap implements Map<String,String> {
     Integer index = _keyIndex.get(key);
     if (index == null)
       throw new IllegalArgumentException("Only preset keys can be assigned. '" + key + "' was not present.");
-    if (_values == null) _values = new String[_keyIndex.size()];
+    expandToFixedSize(index);
     String oldValue = _values[index];
     _values[index] = value;
     return oldValue;
+  }
+
+  private void expandToFixedSize(int requestedIndex) {
+    if (_values == null)
+      _values = new String[_keyIndex.size()];
+    else if (requestedIndex >= _values.length)
+      // must have initialized with a smaller array but now wants to put a value at a higher index
+      _values = Arrays.copyOf(_values, _keyIndex.size());
   }
 
   @Override
@@ -111,14 +128,15 @@ public class FixedSizeStringMap implements Map<String,String> {
 
   @Override
   public Collection<String> values() {
-    return _values == null ? Collections.emptyList() : Arrays.asList(_values);
+    expandToFixedSize(_keyIndex.size());
+    return Arrays.asList(_values);
   }
 
   @Override
   public Set<Entry<String, String>> entrySet() {
     Set<Entry<String,String>> entries = new HashSet<>();
     for (String key : _keyIndex.keySet()) {
-      entries.add(new TwoTuple<String,String>(key, _values == null ? null : _values[_keyIndex.get(key)]));
+      entries.add(new TwoTuple<String,String>(key, get(key)));
     }
     return entries;
   }
