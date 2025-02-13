9ef9a71ff3ec5f5f0efc3c3ddcb65335a8ea3fe4
Ryan Doherty <tech@conical.org>
2025-02-12 20:39:18 -0500

    Add and improve tests

--- Cache/src/test/java/org/gusdb/fgputil/cache/DiskCacheTest.java
+++ Cache/src/test/java/org/gusdb/fgputil/cache/DiskCacheTest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,12 +16,14 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 import org.gusdb.fgputil.IoUtil;
 import org.gusdb.fgputil.Timer;
 import org.gusdb.fgputil.cache.disk.DirectoryLock.DirectoryLockTimeoutException;
 import org.gusdb.fgputil.cache.disk.OnDiskCache;
 import org.gusdb.fgputil.cache.disk.OnDiskCache.Overwrite;
+import org.gusdb.fgputil.functional.FunctionalInterfaces.FunctionWithException;
 import org.gusdb.fgputil.runtime.ThreadUtil;
 import org.junit.Assert;
 import org.junit.Test;
@@ -31,7 +34,7 @@ public class DiskCacheTest {
   private static final int NUM_CALLS = 100;
   private static final int NUM_THREADS = 10;
   private static final int NUM_UNIQUE_KEYS = 10;
-  private static final int NUM_LINES_PER_FILE = 10;
+  private static final Integer NUM_LINES_PER_FILE = 10;
   private static final long TIMEOUT_MILLIS = 5000;
   private static final long POLL_FREQUENCY_MILLIS = 1000;
   private static final int WRITE_DELAY_MULTIPLIER = 50;
@@ -50,12 +53,25 @@ public class DiskCacheTest {
       Random random = new Random();
       OnDiskCache cache = new OnDiskCache(parentDir, TIMEOUT_MILLIS, POLL_FREQUENCY_MILLIS);
 
+      FunctionWithException<Path, Integer> lineCounterVisitor = dir -> {
+        File file = Paths.get(dir.toString(), "blah").toFile();
+        //System.out.println("Reading from " + file);
+        int numLines = 0;
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+          while (in.ready()) {
+            in.readLine();
+            numLines++;
+          }
+        }
+        return numLines;
+      };
+
       List<Future<Boolean>> results = new ArrayList<>();
       for (int i = 0; i < NUM_CALLS; i++) {
         int randomDigit = random.nextInt(NUM_UNIQUE_KEYS);
         String digitString = String.valueOf(randomDigit);
         results.add(exec.submit(() -> {
-          cache.populateAndProcessContent(
+          Assert.assertEquals(NUM_LINES_PER_FILE, cache.populateAndProcessContent(
               digitString, // cache key
               dir -> {
                 ThreadUtil.sleep(randomDigit * WRITE_DELAY_MULTIPLIER);
@@ -67,19 +83,8 @@ public class DiskCacheTest {
                   }
                 }
               },
-              dir -> {
-                File file = Paths.get(dir.toString(), "blah").toFile();
-                //System.out.println("Reading from " + file);
-                try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-                  int lines = 0;
-                  while (in.ready()) {
-                    in.readLine();
-                    lines++;
-                  }
-                  Assert.assertEquals(NUM_LINES_PER_FILE, lines);
-                }
-              },
-              randomDigit < 4 ? Overwrite.NO : randomDigit < 8 ? Overwrite.YES : Overwrite.IF_FAILED);
+              lineCounterVisitor,
+              randomDigit < 4 ? Overwrite.NO : randomDigit < 8 ? Overwrite.YES : Overwrite.IF_FAILED));
           return true;
         }));
       }
@@ -91,7 +96,7 @@ public class DiskCacheTest {
         }
         catch (ExecutionException e) {
           if (e.getCause() != null && e.getCause() instanceof DirectoryLockTimeoutException) {
-            System.err.println("One of the locks timed out in the get call!");
+            //System.err.println("One of the locks timed out in the get call!");
             numTimeouts++;
           }
           else {
@@ -99,12 +104,44 @@ public class DiskCacheTest {
           }
         }
       }
+      // informational; some timeouts are expected based on the parameters above
       System.err.println("Number of lock timeouts: " + numTimeouts);
+
+      // test visiting the entries
+      for (int i = 0; i < NUM_UNIQUE_KEYS; i++) {
+        Assert.assertEquals(NUM_LINES_PER_FILE, cache.visitContent(String.valueOf(i), lineCounterVisitor));
+      }
+
+      // confirm no lock files exist but parent dir does
+      for (int i = 0; i < NUM_UNIQUE_KEYS; i++) {
+        Assert.assertTrue(Files.isDirectory(Paths.get(parentDir.toString(), String.valueOf(i))));
+        Assert.assertFalse(Files.exists(Paths.get(parentDir.toString(), String.valueOf(i), ".lock")));
+      }
+
+      // delete an entry
+      cache.removeEntry("1");
+      Assert.assertFalse(Files.exists(Paths.get(parentDir.toString(), "1")));
+
+      // delete the rest of the entries
+      cache.removeAllEntries();
+      long remainingEntries = Files.list(parentDir).collect(Collectors.counting());
+      Assert.assertEquals(0, remainingEntries);
     }
     finally {
       exec.shutdown();
       IoUtil.deleteDirectoryTree(parentDir);
     }
     System.out.println("Test took " + t.getElapsedString());
+  }
+
+  public boolean isValidAndUpToDate(Path parentDir, String cacheKey, String newDataDigestValue) throws Exception {
+    OnDiskCache cache = new OnDiskCache(parentDir, TIMEOUT_MILLIS, POLL_FREQUENCY_MILLIS);
+    return cache.visitContent(cacheKey, dir -> {
+      if (!OnDiskCache.isEntryComplete(dir)) return false;
+      try (Reader in = new FileReader(dir.resolve("digest.txt").toFile())) {
+        String oldDigest = IoUtil.readAllChars(in);
+        return  newDataDigestValue.equals(oldDigest);
+      }
+    });
   }
 }
