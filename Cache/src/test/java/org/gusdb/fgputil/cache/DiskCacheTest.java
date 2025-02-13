0b4992954f8e5ac12398218391496f45342d648e
Ryan Doherty <tech@conical.org>
2025-02-12 21:49:31 -0500

    Test tmp dir cleanup

--- Cache/src/test/java/org/gusdb/fgputil/cache/DiskCacheTest.java
+++ Cache/src/test/java/org/gusdb/fgputil/cache/DiskCacheTest.java
@@ -126,6 +126,9 @@ public class DiskCacheTest {
       cache.removeAllEntries();
       long remainingEntries = Files.list(parentDir).collect(Collectors.counting());
       Assert.assertEquals(0, remainingEntries);
+
+      // clean up parent dir
+      Files.delete(parentDir);
     }
     finally {
       exec.shutdown();
