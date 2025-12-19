cac0e7af54d31cfab0fb23741a81308b859d1e9d
steve-fischer-200 <sfischer@upenn.edu>
2025-12-19 14:47:10 -0500

    speed postgres paged queries

--- Db/src/main/java/org/gusdb/fgputil/db/platform/PostgreSQL.java
+++ Db/src/main/java/org/gusdb/fgputil/db/platform/PostgreSQL.java
@@ -150,11 +150,11 @@ public class PostgreSQL extends DBPlatform {
   public String getPagedSql(String sql, int startIndex, int endIndex, boolean includeRowIndex) {
     String rowIndex = includeRowIndex ? ", " + getRowNumberColumn() + " as row_index " : "";
     StringBuilder buffer = new StringBuilder("SELECT f.*" + rowIndex + " FROM ");
-    buffer.append("(").append(sql).append(") f ");
+    buffer.append("(").append(sql);
     if (endIndex > -1) {
       buffer.append(" LIMIT ").append(endIndex - startIndex + 1);
     }
-    buffer.append(" OFFSET ").append(startIndex - 1);
+    buffer.append(" OFFSET ").append(startIndex - 1).append(") f ");
     return buffer.toString();
   }
 
