416318a7eae79fe44431ea2ccc6ff812ab290fdb
Ryan Doherty <tech@conical.org>
2026-03-10 18:44:29 -0400

    Add runtime function in use in WDK

--- Core/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
+++ Core/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
@@ -1,7 +1,18 @@
 package org.gusdb.fgputil.runtime;
 
-import java.lang.reflect.Field;
+import static org.gusdb.fgputil.functional.Functions.swallowAndGet;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class RuntimeUtil {
@@ -26,5 +37,62 @@ public class RuntimeUtil {
       pid = -1;
     }
     return pid;
+  }
+
+  public static void executeAndLogOutput(List<String> command, Map<String,String> environment,
+      Logger logger, Level logLevel, Optional<Duration> processTimeout) {
+    Thread logMonitorThread = null;
+    try {
+      LOG.info("Starting subprocess with command: " + String.join(" ", command));
+      // start the process
+      ProcessBuilder processBuilder = new ProcessBuilder()
+          .command(command)
+          .redirectErrorStream(true);
+      processBuilder.environment().putAll(environment);
+      Process process = processBuilder.start();
+
+      // start a thread to stream output to the passed Logger (if level allows)
+      if (logger.isEnabledFor(logLevel)) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        logMonitorThread = new Thread(() -> {
+          try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+              logger.log(logLevel, ">> " + line);
+            }
+          }
+          catch (IOException e) {
+            logger.log(logLevel, "Parent process warning: could not read subprocess output", e);
+          }
+        });
+        logMonitorThread.start();
+      }
+
+      // wait for process to finish in this thread
+      boolean exitWithoutTimeout = processTimeout
+          .map(duration -> swallowAndGet(() -> process.waitFor(duration.toMillis(), TimeUnit.MILLISECONDS)))
+          .orElse(process.waitFor() - process.exitValue() == 0); // always true
+
+      if (exitWithoutTimeout) {
+        if (process.exitValue() != 0) {
+          throw new RuntimeException("Subprocess exited with error code " + process.exitValue());
+        }
+      }
+      else {
+        throw new RuntimeException("Subprocess timed out before completion");
+      }
+    }
+    catch (InterruptedException e) {
+      throw new RuntimeException("Subprocess was interrupted before completion", e);
+    }
+    catch (IOException e) {
+      throw new RuntimeException("Error occurred while executing subprocess", e);
+    }
+    finally {
+      // kill log monitor thread if still active
+      if (logMonitorThread != null && logMonitorThread.isAlive()) {
+        logMonitorThread.interrupt();
+      }
+    }
   }
 }
