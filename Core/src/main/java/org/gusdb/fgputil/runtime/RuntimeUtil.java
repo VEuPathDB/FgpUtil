42d0375a1f59d9b9ccbe8e5c886a336ef6ea09d0
Ryan Doherty <tech@conical.org>
2026-04-27 09:23:51 -0400

    Change API for subprocess execution - add output consumption flexibility

--- Core/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
+++ Core/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
@@ -13,8 +13,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class RuntimeUtil {
@@ -58,8 +58,7 @@ public class RuntimeUtil {
    * @param command command used to execute the subprocess
    * @param environment supplemental environment variables (will inherit env from this process)
    * @param stdinFile optional file to send to subprocess's stdin stream
-   * @param logger Logger to use to log subprocess output
-   * @param logLevel log level used to log subprocess output
+   * @param outputConsumer consumer which will be passed stderr output, and also stdout if stdoutFile is missing
    * @param stdoutFile optional file to write subprocess stdout to.  If not specified,
    * stdout/stderr will both be written to the Logger
    * @param processTimeout optional duration after which subprocess will be determined to have
@@ -67,15 +66,14 @@ public class RuntimeUtil {
    * avoid a zombie process), and return an empty optional.
    * @return exit value of the subprocess or empty if subprocess times out
    */
-  public static Optional<Integer> executeSubprocessAndLogOutput(
+  public static Optional<Integer> executeSubprocess(
       List<String> command,
       Map<String,String> environment,
       Optional<File> stdinFile,
-      Logger logger,
-      Level logLevel,
+      Consumer<String> outputConsumer,
       Optional<File> stdoutFile,
       Optional<Duration> processTimeout) {
-    Thread logMonitorThread = null;
+    Thread outputConsumptionThread = null;
     try {
       LOG.info("Starting subprocess with command: " + String.join(" ", command));
 
@@ -100,19 +98,19 @@ public class RuntimeUtil {
 
       // start a thread to stream output to the passed Logger (if level allows)
       InputStream streamToLog = stdoutFile.isPresent() ? process.getErrorStream() : process.getInputStream();
-      logMonitorThread = new Thread(() -> {
+      outputConsumptionThread = new Thread(() -> {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(streamToLog))) {
           String line;
           while ((line = reader.readLine()) != null) {
             if (Thread.interrupted()) return;
-            logger.log(logLevel, ">> " + line);
+            outputConsumer.accept(line);
           }
         }
         catch (IOException e) {
           LOG.error("Parent process warning: could not read subprocess output", e);
         }
       });
-      logMonitorThread.start();
+      outputConsumptionThread.start();
 
       // wait for process to finish in this thread
       boolean exitWithoutTimeout = processTimeout
@@ -120,7 +118,7 @@ public class RuntimeUtil {
           .orElse(process.waitFor() - process.exitValue() == 0); // always true
 
       if (exitWithoutTimeout) {
-        waitForLoggingThread(logMonitorThread);
+        waitForConsumptionThread(outputConsumptionThread);
         int exitValue = process.exitValue();
         LOG.info("Subprocess exited with exit code: " + exitValue);
         return Optional.of(exitValue);
@@ -135,7 +133,7 @@ public class RuntimeUtil {
           process.destroyForcibly(); // escalate
           process.waitFor(); // ensure it's actually gone
         }
-        waitForLoggingThread(logMonitorThread);
+        waitForConsumptionThread(outputConsumptionThread);
         return Optional.empty();
       }
     }
@@ -153,13 +151,13 @@ public class RuntimeUtil {
     }
     finally {
       // kill log monitor thread if still active
-      if (logMonitorThread != null && logMonitorThread.isAlive()) {
-        logMonitorThread.interrupt();
+      if (outputConsumptionThread != null && outputConsumptionThread.isAlive()) {
+        outputConsumptionThread.interrupt();
       }
     }
   }
 
-  private static void waitForLoggingThread(Thread logMonitorThread) throws InterruptedException {
+  private static void waitForConsumptionThread(Thread logMonitorThread) throws InterruptedException {
     // Wait for the thread to finish processing the subprocess's output (max 500ms).
     //   This should not take long because the system output buffer is not that big and
     //   the thread has been streaming the data out throughout the subprocess lifespan.
